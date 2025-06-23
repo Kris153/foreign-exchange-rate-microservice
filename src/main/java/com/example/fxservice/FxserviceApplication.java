a99f981408ccb9fad4c4867eaf423dcf4012a1a4
Kris <kris.nikolov@abv.bg>
2025-06-23 14:57:27 +0300

    Initial commit

---
+++ src/main/java/com/example/fxservice/FxserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxserviceApplication.class, args);
+	}
+
+}
