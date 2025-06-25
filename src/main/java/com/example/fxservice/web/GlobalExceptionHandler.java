bf4c00246506f77c3777b52d8c8196b9192e1f87
Kris <kris.nikolov@abv.bg>
2025-06-25 14:58:52 +0300

    Exception handling.

---
+++ src/main/java/com/example/fxservice/web/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.fxservice.web;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String handleAllExceptions() {
+        return "/error";
+    }
+}
+
